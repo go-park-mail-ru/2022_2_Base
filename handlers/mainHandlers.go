0624555a9e58fad11e5ce526ac125411d58f668a
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-10-05 18:51:29 +0300

    2

--- handlers/mainHandlers.go
+++ handlers/mainHandlers.go
@@ -50,7 +50,7 @@ func NewProductHandler() *ProductHandler {
 // @Failure 500 {object} model.Error "Internal Server Error - Request is valid but operation failed at server side"
 // @Router /login [post, options]
 func (api *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://89.208.198.137:8081/")
+	w.Header().Set("Access-Control-Allow-Origin", "http://89.208.198.137:8081")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	if r.Method == http.MethodOptions {
 		return
