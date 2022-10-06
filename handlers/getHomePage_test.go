7f62326c9d5865931b57a90b1698aca52c48959e
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-10-06 16:01:06 +0300

    new photos

--- handlers/getHomePage_test.go
+++ handlers/getHomePage_test.go
@@ -24,7 +24,7 @@ func TestGetHomePage(t *testing.T) {
 		handler.ServeHTTP(rr, req)
 		assert.Equal(t, http.StatusOK, rr.Code)
 
-		var expected = `{"body":[{"id":0,"name":"phone","description":"good phone","price":10000,"lowprice":8000,"rating":5,"imgsrc":"https://s.ek.ua/jpg_zoom1/2090045.jpg"},{"id":1,"name":"notebook","description":"goood","price":70000,"lowprice":55000,"rating":4.3,"imgsrc":"https://fainaidea.com/wp-content/uploads/2016/11/324987.jpg"},{"id":2,"name":"ipad","description":"old","price":3000,"lowprice":3000,"rating":1,"imgsrc":"https://cdn-files.kimovil.com/default/0005/17/thumb_416447_default_big.jpeg"}]}`
+		var expected = `{"body":[{"id":0,"name":"phone","description":"good phone","price":10000,"lowprice":8000,"rating":5,"imgsrc":"https://media.4rgos.it/i/Argos/9520055_R_Z001A?w=1500\u0026h=880\u0026qlt=70\u0026fmt=webp"},{"id":1,"name":"notebook","description":"goood","price":70000,"lowprice":55000,"rating":4.3,"imgsrc":"https://www.notebookcheck-ru.com/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1_.JPG"},{"id":2,"name":"ipad","description":"old","price":3000,"lowprice":3000,"rating":1,"imgsrc":"https://cdn-files.kimovil.com/default/0005/17/thumb_416447_default_big.jpeg"}]}`
 		assert.Equal(t, rr.Body.String(), expected+"\n")
 	})
 }
