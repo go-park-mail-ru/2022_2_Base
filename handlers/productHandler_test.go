b4145d134e7b21b040cc50a1c93bb322f7288df3
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-10-06 18:38:25 +0300

    new products

--- handlers/productHandler_test.go
+++ handlers/productHandler_test.go
@@ -14,13 +14,19 @@ var cases = []struct {
 	err  error
 }{
 	{[]*model.Product{
-		{ID: 0, Name: "phone", Description: "good phone", Price: 10000, DiscountPrice: 8000, Rating: 5, Imgsrc: "https://media.4rgos.it/i/Argos/9520055_R_Z001A?w=1500&h=880&qlt=70&fmt=webp"},
-		{ID: 1, Name: "notebook", Description: "goood", Price: 70000, DiscountPrice: 55000, Rating: 4.3, Imgsrc: "https://www.notebookcheck-ru.com/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1_.JPG"},
-		{ID: 2, Name: "ipad", Description: "old", Price: 3000, DiscountPrice: 3000, Rating: 1, Imgsrc: "https://cdn-files.kimovil.com/default/0005/17/thumb_416447_default_big.jpeg"},
+		{ID: 0, Name: "Монитор Xiaomi Mi Desktop Monitor 27'' RMMNT27NF (BHR4975EU)", Description: "good", Price: 14999, DiscountPrice: 13999, Rating: 4, Imgsrc: "https://img.mvideo.ru/Big/30058309bb.jpg"},
+		{ID: 1, Name: "Телевизор Haier 55 Smart TV MX", Description: "goood", Price: 59999, DiscountPrice: 41999, Rating: 4.3, Imgsrc: "https://img.mvideo.ru/Big/10030234bb.jpg"},
+		{ID: 2, Name: "Планшет Apple iPad 10.2 Wi-Fi+Cell 64GB Space Grey (MK473)", Description: "old", Price: 49999, DiscountPrice: 49999, Rating: 3.7, Imgsrc: "https://img.mvideo.ru/Pdb/30064043b.jpg"},
+		{ID: 3, Name: "Смартфон Tecno Spark 8с 4+64GB Magnet Black", Description: "good phone", Price: 12999, DiscountPrice: 8999, Rating: 4.5, Imgsrc: "https://img.mvideo.ru/Big/30062036bb.jpg"},
+		{ID: 4, Name: "Смартфон realme GT Master Edition 6+128GB Voyager Grey (RMX3363)", Description: "goood", Price: 29999, DiscountPrice: 21999, Rating: 4.3, Imgsrc: "https://img.mvideo.ru/Big/30058843bb.jpg"},
+		{ID: 5, Name: "Смартфон Apple iPhone 11 128GB White", Description: "old", Price: 62999, DiscountPrice: 54999, Rating: 5, Imgsrc: "https://img.mvideo.ru/Big/30063237bb.jpg"},
 	}, []*model.Product{
-		{ID: 0, Name: "phone", Description: "good phone", Price: 10000, DiscountPrice: 8000, Rating: 5, Imgsrc: "https://media.4rgos.it/i/Argos/9520055_R_Z001A?w=1500&h=880&qlt=70&fmt=webp"},
-		{ID: 1, Name: "notebook", Description: "goood", Price: 70000, DiscountPrice: 55000, Rating: 4.3, Imgsrc: "https://www.notebookcheck-ru.com/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1_.JPG"},
-		{ID: 2, Name: "ipad", Description: "old", Price: 3000, DiscountPrice: 3000, Rating: 1, Imgsrc: "https://cdn-files.kimovil.com/default/0005/17/thumb_416447_default_big.jpeg"},
+		{ID: 0, Name: "Монитор Xiaomi Mi Desktop Monitor 27'' RMMNT27NF (BHR4975EU)", Description: "good", Price: 14999, DiscountPrice: 13999, Rating: 4, Imgsrc: "https://img.mvideo.ru/Big/30058309bb.jpg"},
+		{ID: 1, Name: "Телевизор Haier 55 Smart TV MX", Description: "goood", Price: 59999, DiscountPrice: 41999, Rating: 4.3, Imgsrc: "https://img.mvideo.ru/Big/10030234bb.jpg"},
+		{ID: 2, Name: "Планшет Apple iPad 10.2 Wi-Fi+Cell 64GB Space Grey (MK473)", Description: "old", Price: 49999, DiscountPrice: 49999, Rating: 3.7, Imgsrc: "https://img.mvideo.ru/Pdb/30064043b.jpg"},
+		{ID: 3, Name: "Смартфон Tecno Spark 8с 4+64GB Magnet Black", Description: "good phone", Price: 12999, DiscountPrice: 8999, Rating: 4.5, Imgsrc: "https://img.mvideo.ru/Big/30062036bb.jpg"},
+		{ID: 4, Name: "Смартфон realme GT Master Edition 6+128GB Voyager Grey (RMX3363)", Description: "goood", Price: 29999, DiscountPrice: 21999, Rating: 4.3, Imgsrc: "https://img.mvideo.ru/Big/30058843bb.jpg"},
+		{ID: 5, Name: "Смартфон Apple iPhone 11 128GB White", Description: "old", Price: 62999, DiscountPrice: 54999, Rating: 5, Imgsrc: "https://img.mvideo.ru/Big/30063237bb.jpg"},
 	}, nil},
 }
 
