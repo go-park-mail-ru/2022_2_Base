75663674e791359387a7a6ccad704e7b003e054f
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-12-11 18:53:57 +0300

    added csrf header to access allow headers

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ var PathGetOrders = "/orders"
 var Headers = map[string]string{
 	//"Access-Control-Allow-Origin":      "http://89.208.198.137:8081",
 	"Access-Control-Allow-Credentials": "true",
-	"Access-Control-Allow-Headers":     "Origin, Content-Type, accept",
+	"Access-Control-Allow-Headers":     "Origin, Content-Type, accept, csrf",
 	"Access-Control-Allow-Methods":     "GET, POST, DELETE, OPTIONS",
 	"Content-Type":                     "application/json",
 }
