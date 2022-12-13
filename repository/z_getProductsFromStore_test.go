41f5cc8ae4418656a0d4a7c14d7075a8813b12d3
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-12-13 18:57:10 +0300

    get products test

--- repository/z_getProductsFromStore_test.go
+++ repository/z_getProductsFromStore_test.go
@@ -6,13 +6,7 @@ import (
 	"serv/domain/model"
 	"testing"
 
-	//"serv/usecase"
-
-	//"github.com/driftprogramming/pgxpoolmock"
-
-	//mocks "serv/repository/mocks"
 	"github.com/DATA-DOG/go-sqlmock"
-	//"github.com/pashagolub/pgxmock/v2"
 )
 
 func TestGetProductsFromStore(t *testing.T) {
@@ -22,44 +16,9 @@ func TestGetProductsFromStore(t *testing.T) {
 	}
 	defer db.Close()
 
-	//var itemID int = 1
-
-	// good query
-	// rows := sqlmock.
-	// 	NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(1, "IPhone", "phones", 50000, 50000, 0, nil)
 	expect := []*model.Product{
 		{ID: 1, Name: "IPhone", Category: "phones", Price: 50000, NominalPrice: 50000, Rating: 0, Imgsrc: nil, CommentsCount: nil},
 	}
-
-	// for _, item := range expect {
-	// 	rows = rows.AddRow(item.ID, item.Name, item.Category, item.Price, item.NominalPrice, item.Rating, item.Imgsrc)
-	// }
-
-	// mock.
-	// 	ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE").
-	// 	WithArgs(itemID).
-	// 	WillReturnRows(rows)
-
-	// 	mock.ExpectBegin()
-	// 	mock.ExpectQuery(`
-	// INSERT INTO users (id, login, password, is_super)
-	// VALUES ($1, $2, $3, $4)
-	// RETURNING created_at;`).WithArgs(
-	// 		driver.Value(1),
-	// 		driver.Value("user01"),
-	// 		driver.Value("first-P"),
-	// 		driver.Value(false),
-	// 	).WillReturnError(&pq.Error{Severity: "ERROR", Code: "23505", Message: "duplicate key value violates unique constraint \"users_pkey\"", Detail: "Key (id)=(1) already exists.", Hint: "", Position: "", InternalPosition: "", InternalQuery: "", Where: "", Schema: "public", Table: "users", Column: "", DataTypeName: "", Constraint: "users_pkey", File: "nbtinsert.c", Line: "432", Routine: "_bt_check_unique"})
-	// 	mock.ExpectRollback()
-
-	// mock.ExpectQuery(`
-	// 	SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products
-	// 	WHERE id = $1;`).WithArgs(
-	// 	driver.Value(0),
-	// ).WillReturnRows(func() *sqlmock.Rows {
-	// 	rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(0, "", "", 0, 0, 0, nil)
-	// 	return rr
-	// }())
 
 	mock.
 		ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE id").
@@ -68,13 +27,10 @@ func TestGetProductsFromStore(t *testing.T) {
 			rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(0, "", "", 0, 0, 0, nil)
 			return rr
 		}())
-
-	//mock.ExpectBegin()
 	mock.ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE id >").WithArgs(driver.Value(0), driver.Value(1)).WillReturnRows(func() *sqlmock.Rows {
 		rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(1, "IPhone", "phones", 50000, 50000, 0, nil)
 		return rr
 	}())
-	//mock.ExpectRollback()
 
 	repo := &ProductStore{
 		db: db,
@@ -96,21 +52,202 @@ func TestGetProductsFromStore(t *testing.T) {
 		return
 	}
 
-	// rows = sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "asd")
+	//query error
 
-	// mock.
-	// 	ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE").
-	// 	WithArgs(itemID).
-	// 	WillReturnRows(rows)
+	// rows = sqlmock.NewRows([]string{"id", "name"}).
+	// 	AddRow(1, "asd")
 
-	// _, err = repo.GetProductFromStoreByID(itemID)
-	// if err := mock.ExpectationsWereMet(); err != nil {
-	// 	t.Errorf("there were unfulfilled expectations: %s", err)
-	// 	return
-	// }
-	// if err == nil {
-	// 	t.Errorf("expected error, got nil")
-	// 	return
-	// }
+	mock.
+		ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE").
+		WithArgs(driver.Value(0)).
+		WillReturnRows(func() *sqlmock.Rows {
+			rr := sqlmock.NewRows([]string{"id", "name"}).AddRow(0, "")
+			return rr
+		}())
 
+	_, err = repo.GetProductsFromStore(lastItemID, count, "")
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
+}
+
+// func TestGetProductsFromStoreSortingPriceup(t *testing.T) {
+// 	db, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("cant create mock: %s", err)
+// 	}
+// 	defer db.Close()
+
+// 	expect := []*model.Product{
+// 		{ID: 1, Name: "IPhone", Category: "phones", Price: 50000, NominalPrice: 50000, Rating: 0, Imgsrc: nil, CommentsCount: nil},
+// 	}
+
+// 	var lastItemID int = 0
+// 	var count int = 1
+// 	var sort string = "priceup"
+// 	var lastprice float64 = 0
+
+// 	mock.
+// 		ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE id").
+// 		WithArgs(driver.Value(0)).
+// 		WillReturnRows(func() *sqlmock.Rows {
+// 			rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(0, "", "", 0, 0, 0, nil)
+// 			return rr
+// 		}())
+// 	mock.ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE").WithArgs(lastprice, lastItemID, count).WillReturnRows(func() *sqlmock.Rows {
+// 		rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(1, "IPhone", "phones", 50000, 50000, 0, nil)
+// 		return rr
+// 	}())
+
+// 	repo := &ProductStore{
+// 		db: db,
+// 	}
+// 	items, err := repo.GetProductsFromStore(lastItemID, count, sort)
+// 	if err != nil {
+// 		t.Errorf("unexpected err: %s", err)
+// 		return
+// 	}
+// 	if err := mock.ExpectationsWereMet(); err != nil {
+// 		t.Errorf("there were unfulfilled expectations: %s", err)
+// 		return
+// 	}
+// 	if !reflect.DeepEqual(items[0], expect[0]) {
+// 		t.Errorf("results not match, want %v, have %v", expect[0], items)
+// 		return
+// 	}
+// }
+
+// func TestGetProductsFromStoreSortingPricedown(t *testing.T) {
+// 	db, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("cant create mock: %s", err)
+// 	}
+// 	defer db.Close()
+
+// 	expect := []*model.Product{
+// 		{ID: 1, Name: "IPhone", Category: "phones", Price: 50000, NominalPrice: 50000, Rating: 0, Imgsrc: nil, CommentsCount: nil},
+// 	}
+
+// 	var lastItemID int = 0
+// 	var count int = 1
+// 	var sort string = "pricedown"
+// 	var lastprice float64 = 1e10
+
+// 	mock.
+// 		ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE id").
+// 		WithArgs(driver.Value(0)).
+// 		WillReturnRows(func() *sqlmock.Rows {
+// 			rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(0, "", "", 0, 0, 0, nil)
+// 			return rr
+// 		}())
+// 	mock.ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE").WithArgs(lastprice, lastItemID, count).WillReturnRows(func() *sqlmock.Rows {
+// 		rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(1, "IPhone", "phones", 50000, 50000, 0, nil)
+// 		return rr
+// 	}())
+
+// 	repo := &ProductStore{
+// 		db: db,
+// 	}
+// 	items, err := repo.GetProductsFromStore(lastItemID, count, sort)
+// 	if err != nil {
+// 		t.Errorf("unexpected err: %s", err)
+// 		return
+// 	}
+// 	if err := mock.ExpectationsWereMet(); err != nil {
+// 		t.Errorf("there were unfulfilled expectations: %s", err)
+// 		return
+// 	}
+// 	if !reflect.DeepEqual(items[0], expect[0]) {
+// 		t.Errorf("results not match, want %v, have %v", expect[0], items)
+// 		return
+// 	}
+// }
+
+// var casesGetProductsFromStoreSortings = []struct {
+// 	lastItemID   int
+// 	count        int
+// 	sort         string
+// 	lastprice    float64
+// 	queryProduct model.Product
+// }{
+// 	{0, 1, "priceup", 0, model.Product{ID: 1, Name: "IPhone", Category: "phones", Price: 50000, NominalPrice: 50000, Rating: 0, Imgsrc: nil, CommentsCount: nil}},
+// 	{0, 1, "pricedown", 1e10, model.Product{ID: 1, Name: "IPhone", Category: "phones", Price: 50000, NominalPrice: 50000, Rating: 0, Imgsrc: nil, CommentsCount: nil}},
+// 	{0, 1, "ratingup", 0, model.Product{ID: 1, Name: "IPhone", Category: "phones", Price: 0, NominalPrice: 0, Rating: 0, Imgsrc: nil, CommentsCount: nil}},
+// 	{1e9, 1, "ratingdown", 0, model.Product{ID: 1, Name: "IPhone", Category: "phones", Price: 0, NominalPrice: 0, Rating: 10, Imgsrc: nil, CommentsCount: nil}},
+// }
+
+var casesGetProductsFromStoreSortings = []struct {
+	lastItemID int
+	count      int
+	sort       string
+	lastprice  float64
+	lastrating float64
+}{
+	{0, 1, "priceup", 0, 0},
+	{0, 1, "pricedown", 1e10, 0},
+	{0, 1, "ratingup", 0, 0},
+	{1e9, 1, "ratingdown", 0, 10},
+}
+
+func TestGetProductsFromStoreSortings(t *testing.T) {
+	for _, c := range casesGetProductsFromStoreSortings {
+		t.Run("tests", func(t *testing.T) {
+			t.Parallel()
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("cant create mock: %s", err)
+			}
+			defer db.Close()
+
+			expect := []*model.Product{
+				{ID: 1, Name: "IPhone", Category: "phones", Price: 50000, NominalPrice: 50000, Rating: 0, Imgsrc: nil, CommentsCount: nil},
+			}
+
+			var lastItemID int = c.lastItemID
+			var count int = c.count
+			var sort string = c.sort
+			//var lastprice float64 = c.lastprice
+
+			var lastparam float64 = 0
+			if c.lastprice > 0 {
+				lastparam = c.lastprice
+			} else if c.lastrating > 0 {
+				lastparam = c.lastrating
+			}
+
+			mock.
+				ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE id").
+				WithArgs(driver.Value(0)).
+				WillReturnRows(func() *sqlmock.Rows {
+					rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(0, "", "", 0, 0, 0, nil)
+					return rr
+				}())
+			mock.ExpectQuery("SELECT id, name, category, price, nominalprice, rating, imgsrc FROM products WHERE").WithArgs(lastparam, lastItemID, count).WillReturnRows(func() *sqlmock.Rows {
+				rr := sqlmock.NewRows([]string{"id", "name", "category", "price", "nominalprice", "rating", "imgsrc"}).AddRow(1, "IPhone", "phones", 50000, 50000, 0, nil)
+				return rr
+			}())
+
+			repo := &ProductStore{
+				db: db,
+			}
+			items, err := repo.GetProductsFromStore(0, count, sort)
+			if err != nil {
+				t.Errorf("unexpected err: %s", err)
+				return
+			}
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+				return
+			}
+			if !reflect.DeepEqual(items[0], expect[0]) {
+				t.Errorf("results not match, want %v, have %v", expect[0], items)
+				return
+			}
+		})
+	}
 }
