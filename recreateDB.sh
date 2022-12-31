29ea180af8a19145dce527be946d8a6b44d083a5
Aint <pinevichv@gmail.com>
2022-12-31 16:55:40 +0300

    creation of service user

--- recreateDB.sh
+++ recreateDB.sh
@@ -1,11 +1,8 @@
-#sudo docker exec -it postgres bash
-#docker cp ./_postgres postgres:/
 printf "Copy scripts:\n"
 sudo docker cp ./_postgres postgres:/
 printf "DROP TABLE:\n"
-sudo docker exec -it postgres psql -U spuser -d base -a -f ./_postgres/drop.sql -S | grep -E "(NOTICE|ERROR)" && printf "\n"
+sudo docker exec -it postgres psql -U ${DB_USER} -d ${TEST_DB_NAME} -a -f ./_postgres/drop.sql -S | grep -E "(NOTICE|ERROR)" && printf "\n"
 printf "CREATE TABLE:\n"
-sudo docker exec -it postgres psql -U spuser -d base -a -f ./_postgres/init.sql | grep -E "(NOTICE|ERROR)" && printf "\n"
+sudo docker exec -it postgres psql -U ${DB_USER} -d ${TEST_DB_NAME} -a -f ./_postgres/init.sql | grep -E "(NOTICE|ERROR)" && printf "\n"
 printf "POPULTE TABLE:\n"
-sudo docker exec -it postgres psql -U spuser -d base -a -f ./_postgres/populate.sql  | grep -E "(NOTICE|ERROR)" && printf "\n"
-#sudo docker exec -it postgres psql -U spuser --dbname=base
+sudo docker exec -it postgres psql -U ${DB_USER} -d ${TEST_DB_NAME} -a -f ./_postgres/populate.sql  | grep -E "(NOTICE|ERROR)" && printf "\n"
