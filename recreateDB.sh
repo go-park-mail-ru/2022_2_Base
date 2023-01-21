ceae641757236f9ae2995bb59156efc616d0cced
Aint <pinevichv@gmail.com>
2023-01-22 00:18:48 +0300

    update docker compose

--- recreateDB.sh
+++ recreateDB.sh
@@ -1,8 +1,8 @@
 printf "Copy scripts:\n"
 sudo docker cp ./_postgres postgres:/
 printf "DROP TABLE:\n"
-sudo docker exec -it postgres psql -U ${TEST_DB_USER} -d ${TEST_DB_NAME} -a -f ./_postgres/drop.sql -S | grep -E "(NOTICE|ERROR)" && printf "\n"
+sudo docker exec -it postgres psql -U spuser -d base -a -f ./_postgres/drop.sql -S | grep -E "(NOTICE|ERROR)" && printf "\n"
 printf "CREATE TABLE:\n"
-sudo docker exec -it postgres psql -U ${TEST_DB_USER} -d ${TEST_DB_NAME} -a -f ./_postgres/init.sql | grep -E "(NOTICE|ERROR)" && printf "\n"
+sudo docker exec -it postgres psql -U spuser -d base -a -f ./_postgres/init.sql | grep -E "(NOTICE|ERROR)" && printf "\n"
 printf "POPULTE TABLE:\n"
-sudo docker exec -it postgres psql -U ${TEST_DB_USER} -d ${TEST_DB_NAME} -a -f ./_postgres/populate.sql  | grep -E "(NOTICE|ERROR)" && printf "\n"
+sudo docker exec -it postgres psql -U spuser -d base -a -f ./_postgres/populate.sql  | grep -E "(NOTICE|ERROR)" && printf "\n"
